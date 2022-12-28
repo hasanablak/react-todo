a860c55e8d6778e90907dc50ce7ef4cf04f2a0e7
Hasan Ablak <0hasanablak@gmail.com>
2022-12-28 16:20:37 +0300

    Uygulama içerisinde kullanacağım iconları ekledim

---
+++ src/assets/icons/index.js
@@ -0,0 +1,10 @@
+
+
+export { default as AddUser } from "./add-user"
+export { default as Close } from "./close"
+export { default as Edit } from "./edit"
+export { default as Pluse } from "./pluse"
+export { default as Search } from "./search"
+export { default as Trash } from "./trash"
+export { default as Check } from "./check"
+export { default as Uncheck } from "./uncheck"
