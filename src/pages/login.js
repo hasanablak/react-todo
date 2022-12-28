bfaa1a00ae20f088f588b6d33604d24d46268df6
Hasan Ablak <0hasanablak@gmail.com>
2022-12-28 23:09:32 +0300

    Login ve Register sayfalarında güncelleme
    Login ve Register sayfalarını düzenleyerek onları hem daha kullanışlı bir hale getiriyorum hemde onlara "action" ekliyorum yani axios ve hata mesajlarını eklemeye çalışıyorum

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,14 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
+import {useState} from "react"
+
 export default function Login() {
+
+	const [credentinal, setCredentinal] = useState({ email:"", password:""});
+
+	const sendLoginForm = () => {
+		
+	}
+
 	return (
 		<div className="row d-flex justify-content-center align-items-center h-100">
 				<div className="col col-xl-8">
@@ -9,16 +18,30 @@ export default function Login() {
 							<div className="card justify-content-center align-items-center p-3 w-100">
 								<form className="d-flex mb-4 flex-column">
 									<div className="form-outline flex-fill">
-										<label className="form-label">E-mail</label>
-										<input type="text" id="form3" className="form-control form-control-lg" />
+										<label className="form-label" htmlFor="email">E-mail</label>
+										<input
+											required 
+											pattern="[^@]+@[^@]+\.[a-zA-Z]{2,6}"
+											type="text"
+											id="email"
+											onChange={(v) => setCredentinal(f => ({ ...f, email: v }))}
+											className="form-control form-control-lg" />
 									</div>
 									<div className="form-outline flex-fill mt-3">
-										<label className="form-label">Surname</label>
-										<input type="text" id="form3" className="form-control form-control-lg" />
+										<label className="form-label" htmlFor="password">Password</label>
+										<input
+											required
+											type="text"
+											id="password"
+											onChange={(v) => setCredentinal(f => ({ ...f, password: v }))}
+											className="form-control form-control-lg" />
 									</div>
 									<div className="form-outline flex-fill my-3 mt-3">
-										<input type="submit" id="form3" className="form-control form-control-md"
-											value="Login" />
+										<input
+										type="submit"
+										className="form-control form-control-md"
+										value="Login"
+										onClick={sendLoginForm} />
 									</div>
 									<span>
 										<Link to="register">Don't have an account?</Link>
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,5 +1,24 @@
-import {Link} from "react-router-dom"
-export default function Register() {
+import { Link } from "react-router-dom"
+import {useState} from "react"
+import axios from "axios";
+
+export default function Login() {
+
+	const [credenditinal, setCredentinal] = useState({name:"", email:"", password:""});
+
+	const sendRegisterForm = (e) => {
+		e.preventDefault();
+
+		axios.post(process.env.REACT_APP_API_URL+"/register", credenditinal)
+			.then((e) => {
+				console.log(e);
+			})
+			.catch((e) => {
+			console.log(e)
+		})
+		
+	}
+
 	return (
 		<div className="row d-flex justify-content-center align-items-center h-100">
 				<div className="col col-xl-8">
@@ -7,22 +26,40 @@ export default function Register() {
 						<div className="card-body p-5">
 							<h6 className="mb-3 header">Register</h6>
 							<div className="card justify-content-center align-items-center p-3 w-100">
-								<form className="d-flex mb-4 flex-column">
+								<form className="d-flex mb-4 flex-column" onSubmit={sendRegisterForm}>
 									<div className="form-outline flex-fill">
-										<label className="form-label">E-mail</label>
-										<input type="text" id="form3" className="form-control form-control-lg" />
+										<label className="form-label" htmlFor="name">Full name</label>
+										<input
+											 
+											type="text"
+											id="name"
+											onChange={(v) => setCredentinal(f => ({ ...f, name: v.target.value }))}
+											className="form-control form-control-lg" />
 									</div>
 									<div className="form-outline flex-fill mt-3">
-										<label className="form-label">Name</label>
-										<input type="text" id="form3" className="form-control form-control-lg" />
+										<label className="form-label" htmlFor="email">E-mail</label>
+										<input
+											required 
+											pattern="[^@]+@[^@]+\.[a-zA-Z]{2,6}"
+											type="text"
+											id="email"
+											onChange={(v) => setCredentinal(f => ({ ...f, email: v.target.value }))}
+											className="form-control form-control-lg" />
 									</div>
 									<div className="form-outline flex-fill mt-3">
-										<label className="form-label">Password</label>
-										<input type="password" id="form3" className="form-control form-control-lg" />
+										<label className="form-label" htmlFor="password">Password</label>
+										<input
+											required
+											type="password"
+											id="password"
+											onChange={(v) => setCredentinal(f => ({ ...f, password: v.target.value }))}
+											className="form-control form-control-lg" />
 									</div>
 									<div className="form-outline flex-fill my-3 mt-3">
-										<input type="submit" id="form3" className="form-control form-control-md"
-											value="Register" />
+										<input
+										type="submit"
+										className="form-control form-control-md"
+										value="Register" />
 									</div>
 									<span>
 										<Link to="/">Do you have an account?</Link>
