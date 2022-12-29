bfed0d54e6d264c351102c0ca4469146e4094016
Hasan Ablak <0hasanablak@gmail.com>
2022-12-29 13:19:57 +0300

    Login sayfası üzerinde geliştirmeler
    Login sayfası neredeyse bitti.
    
    Url üzerinden gelen email parametresini kontrol ediyorum eğer var ise onu state'imin içerisine alıyorum.
    
    Burada yine axios kullandım, laravel tarafından dönen validatinları axios dosyamın içerisindeki configirasyon hallediyor.
    Form validation için ise default html form validation kullandım required olan inputlar patterna göre doldurulmadan form submit işlemi gerçekleşmiyor.
    
    Backend tarafından 200 kodu geldi ise kullanıcımızın token değerlerini set etmesi için setAuth store'unu dispatch ettiriyorum ve sonrasında da dashboard sayfasına yönlendiriyorum.
    
    Backend tarafında 200'ün başka bir açıklaması olmadığı için ayriyetten bir kontrole gerek duymadım fakat gerek duyulursa elbette buraya ekleme yapılabilir

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,12 +1,35 @@
-import { Link } from "react-router-dom"
-import {useState} from "react"
+import { Link, useNavigate, useSearchParams } from "react-router-dom"
+import {useState, useEffect} from "react"
+import { useDispatch } from "react-redux";
+import { setAuth } from "../stores/auth";
+import axios from "axios";
 
 export default function Login() {
 
-	const [credentinal, setCredentinal] = useState({ email:"", password:""});
+	const [credenditinal, setCredentinal] = useState({email:"", password:""});
+	const [searchParams] = useSearchParams();
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-	const sendLoginForm = () => {
-		
+	useEffect(() => {
+		if (searchParams.get("email")) {
+			setCredentinal(f => ({ ...f, email: searchParams.get("email") }));
+		}
+	}, [searchParams.get("email")]);
+
+	const sendLoginForm = async (e) => {
+		e.preventDefault();
+
+		await axios.post(process.env.REACT_APP_API_URL + "/login", credenditinal)
+			.then((e) => {
+				loginWasSuccessfully(e)
+			});
+
+	}
+
+	const loginWasSuccessfully = async (e)  => {
+		await dispatch(setAuth(e.data));
+		navigate("/dashboard");
 	}
 
 	return (
@@ -16,32 +39,31 @@ export default function Login() {
 						<div className="card-body p-5">
 							<h6 className="mb-3 header">Login</h6>
 							<div className="card justify-content-center align-items-center p-3 w-100">
-								<form className="d-flex mb-4 flex-column">
-									<div className="form-outline flex-fill">
+								<form className="d-flex mb-4 flex-column" onSubmit={sendLoginForm}>
+									<div className="form-outline flex-fill mt-3">
 										<label className="form-label" htmlFor="email">E-mail</label>
 										<input
 											required 
 											pattern="[^@]+@[^@]+\.[a-zA-Z]{2,6}"
 											type="text"
 											id="email"
-											onChange={(v) => setCredentinal(f => ({ ...f, email: v }))}
+											onChange={(v) => setCredentinal(f => ({ ...f, email: v.target.value }))}
 											className="form-control form-control-lg" />
 									</div>
 									<div className="form-outline flex-fill mt-3">
 										<label className="form-label" htmlFor="password">Password</label>
 										<input
 											required
-											type="text"
+											type="password"
 											id="password"
-											onChange={(v) => setCredentinal(f => ({ ...f, password: v }))}
+											onChange={(v) => setCredentinal(f => ({ ...f, password: v.target.value }))}
 											className="form-control form-control-lg" />
 									</div>
 									<div className="form-outline flex-fill my-3 mt-3">
 										<input
 										type="submit"
 										className="form-control form-control-md"
-										value="Login"
-										onClick={sendLoginForm} />
+										value="Login" />
 									</div>
 									<span>
 										<Link to="register">Don't have an account?</Link>
