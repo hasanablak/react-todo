4e8a083f0fb8638339cba47cd06cc8ac97f51776
Hasan Ablak <0hasanablak@gmail.com>
2022-12-29 11:20:50 +0300

    Swal kütüphanesini dahil ettim ve tasarımıma uygun olucak şekilde düzenledim

---
+++ src/helpers/modifided-swal.js
@@ -0,0 +1,12 @@
+import Swal from "sweetalert2";
+
+export function ModifidedSwal() {
+	return Swal.mixin({
+		customClass: {
+			popup: 'dashed',
+			confirmButton: 'form-control dashed',
+			cancelButton: 'form-control dashed'
+		},
+		buttonsStyling: false
+	});
+}
