08ea85b4df36a3077670de04fc9af1ec2eaa32a1
Hasan Ablak <0hasanablak@gmail.com>
2022-12-29 13:31:41 +0300

    Axios default header
    setAuth reducer çalıştığında eğer dolu ise bu bir giriş yapma isteği olduğu kabul edildiğinden ve uygulama içerisinde de axios kütüphanesini kullandığımızdan kaynaklı
    axios.default.header üzerinden Authorization durumunu payload'dan gelen token ile tek seferden burada default olarak dolduruyorum.
    
    Böylece uygulama içerisinde setAuth dispatch edildiğinde axios'un kullanıldığı yerlerde Authorization dolu olmuş olacak.
    
    İkinci olarak, kullanıcı sekmeyi kapattı ve state'lerimiz silindi diyelim.
    Uygulama tekrar  kullanıcı için çalıştığında statelerimiz boş olmuş olucak haliyle fakat kullanıcımızın giriş yaptığını biliyoruz, biliyoruz çünkü localStorage içeriisnde sakladığımız token değerlerimiz hala dolu.
    
    Bundan dolayı auth slice'ımızın initialState'ini doldurmak için localStorage üzerinden gelen token'ı kullanıyoruz aynısını user içinde uyguluyoruz.

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,10 +1,10 @@
 import { createSlice } from	'@reduxjs/toolkit'
-
+import axios from "axios"
 export const authSlice = createSlice({
 	name:	'auth',
 	initialState:	{
-		token: "",
-		user: {}
+		token:  localStorage.getItem("token") ? axios.defaults.headers.common['Authorization'] = localStorage.getItem("token") : "",
+		user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {}
 	},
 	reducers:	{
 		setAuth: (state, {payload: payload}) => {
@@ -13,11 +13,13 @@ export const authSlice = createSlice({
 				state.user = payload.user;
 				localStorage.setItem("token", payload.token);
 				localStorage.setItem("user", JSON.stringify(payload.user));
+				axios.defaults.headers.common['Authorization'] = payload.token;
 			} else {
 				state.token = "";
 				state.user = {};
 				localStorage.removeItem("token");
 				localStorage.removeItem("user");
+				axios.defaults.headers.common['Authorization'] = "";
 			}
 		}
 	},
